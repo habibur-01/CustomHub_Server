979095d26e25245d2b349457422495004caf1f02
habibur-01 <h.r.habibur08@gmail.com>
2024-03-16 20:34:42 +0600

    post & get user data

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
 const app = express()
-const port = 3000
+const port = process.env.PORT  || 3000
 
 // Middleware
-app.use(cors())
+app.use(cors({
+    origin:[
+        'http://localhost:5173'
+    ],
+    credentials:true
+}))
 app.use(express.json())
 
 
@@ -25,19 +30,33 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        // create database
+        const userCollection =  client.db("ContestHub").collection("users")
+
+        app.post("/users", async(req, res) => {
+            const user = req.body
+            const result = await userCollection.insertOne(user)
+            res.send(result)
+        })
+        
+        app.get("/users", async(req, res ) => {
+            const result = await userCollection.find().toArray()
+            res.send(result)
+        })
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Contest_Hub!')
 })
 
 app.listen(port, () => {
